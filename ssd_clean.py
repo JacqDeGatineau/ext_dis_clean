b437e6770f23e212c0b85ae6f94c5910ce4f38b0
Henrik Heinonen <henrikheinonen@Neko.local>
2024-12-30 15:25:17 +0200

    change hashing, made the code more time-efficient

--- ssd_clean.py
+++ ssd_clean.py
@@ -3,17 +3,26 @@ import hashlib
 import sys
 import shutil
 
-def hashfile(filename):
+def hashfile(filename, fast=True):
     try:
-        if os.path.isdir(filename):
-            return None  # Skip directories
+        if 'Backups.backupdb' in filename or filename.startswith('/Volumes/.timemachine'):
+            print(f"Skipping Time Machine file: {filename}")
+            return None # skip time machine backups
+
+        if os.path.isdir(filename) or filename.startswith('.'):
+            return None  # Skip directories and system files
+        
+        hasher = hashlib.md5() # not cryptographically secure, but ok for file checksums
         with open (filename, "rb") as f:
-            hasher = hashlib.sha256()
-            while True:
-                data = f.read(1024)
-                if not data:
-                    break
+            if fast:
+                data = f.read(8192) #read first 8kb first to fasten the hashing
                 hasher.update(data)
+            else:
+                while True:
+                    data = f.read(1024)
+                    if not data:
+                        break
+                    hasher.update(data)
             return hasher.hexdigest()
     except Exception as e:
         print(f"Error hashing file {filename}: {e}")
@@ -22,6 +31,12 @@ def hashfile(filename):
 def get_all_files(directory):
     files = []
     for root, dirnames, filenames in os.walk(directory):
+        print(f"Checking {root}")
+        dirnames[:] = [
+            d for d in dirnames 
+            if 'Backups.backupdb' not in d 
+            and not d.startswith('.Apple')
+        ]
         for filename in filenames:
             files.append(os.path.join(root, filename))
         for dirname in dirnames:
@@ -29,20 +44,30 @@ def get_all_files(directory):
     return files
 
 def find_duplicates(dir_1, dir_2):
-   
     dir_1_files = get_all_files(dir_1)
     dir_2_files = get_all_files(dir_2)
 
-    dir_1_hashes = {hashfile(file): file for file in dir_1_files}
-
+    hash_cache = {}
     duplicates = []
     to_remove = []
-    for file in dir_2_files:
+
+    #get hash with caching
+    def get_file_hash(file):
+        if file in hash_cache:
+            return hash_cache[file]
         file_hash = hashfile(file)
+        if file_hash:
+            hash_cache[file] = file_hash
+        return file_hash
+
+    dir_1_hashes = {get_file_hash(file): file for file in dir_1_files}
+
+    for file in dir_2_files:
+        file_hash = get_file_hash(file)
         if file_hash in dir_1_hashes:
             duplicates.append((dir_1_hashes[file_hash], file))
             to_remove.append(file)
-    
+
     return duplicates, to_remove
 
 def record(dupl):
